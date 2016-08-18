ef409d9ddf537c08cabfbeb5408e170ea6da4eb0
Ilana Corson <ilana.corson@gmail.com>
2016-08-18 09:17:18 -0600

    base done with thumbs down

--- app/assets/javascripts/updateIdeas.js
+++ app/assets/javascripts/updateIdeas.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
   var ideaArray = ['swill', 'plausible', 'genius']
 
-$(".title-text").on('blur', function(){
+  $(".title-text").on('blur', function(){
   var ideaId = $(this).parents('tr').data('id')
   var ideaParams = { idea: {
                       id: ideaId,
                       title: $(this).text()
                     }}
-  $.ajax({
-    type: 'PATCH',
-    url: '/api/v1/ideas/' + ideaId,
-    data: ideaParams
+    $.ajax({
+      type: 'PATCH',
+      url: '/api/v1/ideas/' + ideaId,
+      data: ideaParams
     })
   })
 
@@ -27,24 +27,48 @@ $(".title-text").on('blur', function(){
       })
     })
 
-    $("#each-idea").on('click', '.upvote-idea', function(){
-      var ideaId = $(this).parents('tr').data('id')
-      var ideaRow = $(this).parents('tr')
-      var currentQuality = $(this).closest('tr').children('.quality-text').text()
-      var qualityIndex = ideaArray.indexOf(currentQuality)
-      if(qualityIndex !== ideaArray.length - 1) {
-        var newQuality = ideaArray[qualityIndex + 1]
-        var ideaParams = {idea: {id: ideaId,
-        quality: newQuality}}
-        $.ajax({
-          type: 'PATCH',
-          url: '/api/v1/ideas/' + ideaId,
-          data: ideaParams,
-          success: function(){
+  $("#each-idea").on('click', '.upvote-idea', function(){
+    var ideaId = $(this).parents('tr').data('id')
+    var ideaRow = $(this).parents('tr')
+    var currentQuality = $(this).closest('tr').children('.quality-text').text()
+    var qualityIndex = ideaArray.indexOf(currentQuality)
 
-            ideaRow.children('.quality-text').html(newQuality)
-          }
-          })
+    if(qualityIndex !== ideaArray.length - 1) {
+      var newQuality = ideaArray[qualityIndex + 1]
+      var ideaParams = {idea: {id: ideaId,
+      quality: newQuality}}
+
+      $.ajax({
+        type: 'PATCH',
+        url: '/api/v1/ideas/' + ideaId,
+        data: ideaParams,
+        success: function(){
+
+          ideaRow.children('.quality-text').html(newQuality)
         }
-        })
+      })
+    }
+  })
+
+  $("#each-idea").on('click', '.downvote-idea', function(){
+    var ideaId = $(this).parents('tr').data('id')
+    var ideaRow = $(this).parents('tr')
+    var currentQuality = $(this).closest('tr').children('.quality-text').text()
+    var qualityIndex = ideaArray.indexOf(currentQuality)
+
+    if(qualityIndex !== 0) {
+      var newQuality = ideaArray[qualityIndex - 1]
+      var ideaParams = {idea: {id: ideaId,
+      quality: newQuality}}
+
+      $.ajax({
+        type: 'PATCH',
+        url: '/api/v1/ideas/' + ideaId,
+        data: ideaParams,
+        success: function(){
+          ideaRow.children('.quality-text').html(newQuality)
+        }
+      })
+    }
+  })
 })
